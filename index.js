acc49a3d26eb2d85eab3b87d51c488e793009cc8
rahul <rkumar45650@gmail.com>
2024-04-26 12:02:16 +0530

    new html task

---
+++ index.js
@@ -0,0 +1,19 @@
+const slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
+
+function showSlide(n) {
+    slides[currentSlide].classList.remove('active');
+    currentSlide = (n + slides.length) % slides.length;
+    slides[currentSlide].classList.add('active');
+}
+
+function nextSlide() {
+    showSlide(currentSlide + 1);
+}
+
+function prevSlide() {
+    showSlide(currentSlide - 1);
+}
+
+// Automatically change slide every 3 seconds
+setInterval(nextSlide, 10000);
